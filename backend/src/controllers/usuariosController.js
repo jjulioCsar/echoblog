6acff935579f5feb150bed17894707c7340469b9
jjulioCsar <julio.c.silva70@aluno.senai.br>
2024-09-09 09:34:09 -0300

    RF03 finalizada, adicionado zod na RF02

--- backend/src/controllers/usuariosController.js
+++ backend/src/controllers/usuariosController.js
@@ -20,6 +20,32 @@ const createUsuarios = z.object({
     .optional()
     .default("leitor")
 });
+const loginUsuarios = z.object({
+    email: z.string()
+    .email({ message: "O e-mail deve ser um endereço válido." }),
+    senha: z.string()
+})
+
+
+const updateParams = z.object({
+    id: z.string().uuid("ID deve ser um UUID válido.")
+  });
+  
+  const updateBody = z.object({
+    nome: z.string()
+      .min(3, { message: "O nome deve ter pelo menos 3 caracteres." })
+      .max(50, { message: "O nome não pode ter mais de 50 caracteres." })
+      .optional(),
+    
+    email: z.string()
+      .email({ message: "O e-mail deve ser um endereço válido." })
+      .optional(),
+    
+    senha: z.string()
+      .min(8, { message: "A senha deve ter pelo menos 8 caracteres." })
+      .max(12, { message: "A senha não pode ter mais de 12 caracteres." })
+      .optional()
+  });
 
 export const postUsuarios = async (req, res) => {
   const bodyValidation = createUsuarios.safeParse(req.body);
@@ -34,8 +60,8 @@ export const postUsuarios = async (req, res) => {
   const { nome, email, senha, papel } = req.body;
 
   try {
-    const existingUser = await Usuarios.findOne({ where: { email } });
-    if (existingUser) {
+    const emailExiste = await Usuarios.findOne({ where: { email } });
+    if (emailExiste) {
       return res.status(400).json({ msg: "Usuário com este e-mail já existe." });
     }
 
@@ -58,6 +84,15 @@ export const postUsuarios = async (req, res) => {
 };
 
 export const loginUsuario = async (req, res) => {
+    const bodyValidation = loginUsuarios.safeParse(req.body);
+
+  if (!bodyValidation.success) {
+    return res.status(400).json({
+      msg: "Os dados recebidos do corpo da requisição são inválidos",
+      detalhes: formatZodError(bodyValidation.error),
+    });
+  }
+
   const { email, senha } = req.body;
 
   try {
@@ -91,3 +126,59 @@ export const loginUsuario = async (req, res) => {
     });
   }
 };
+
+
+
+export const updateUsuario = async (req, res) => {
+    const paramsValidation = updateParams.safeParse(req.params);
+    if (!paramsValidation.success) {
+      return res.status(400).json({
+        msg: "Os dados recebidos da URL são inválidos",
+        detalhes: formatZodError(paramsValidation.error),
+      });
+    }
+  
+    const bodyValidation = updateBody.safeParse(req.body);
+    if (!bodyValidation.success) {
+      return res.status(400).json({
+        msg: "Os dados recebidos do corpo da requisição são inválidos",
+        detalhes: formatZodError(bodyValidation.error),
+      });
+    }
+  
+    const { id } = req.params;
+    const { nome, email, senha } = req.body;
+  
+    try {
+      if (email) {
+        const emailExiste = await Usuarios.findOne({ where: { email } });
+        if (emailExiste && emailExiste.id !== id) {
+          return res.status(400).json({ msg: "O e-mail já está em uso por outro usuário." });
+        }
+      }
+  
+      const usuarioAtualizado = { nome, email };
+  
+      if (senha) {
+        usuarioAtualizado.senha = await bcrypt.hash(senha, 10);
+      }
+  
+      await Usuarios.update(usuarioAtualizado, { where: { id } });
+  
+      // Buscar o usuário atualizado
+      const usuario = await Usuarios.findOne({ where: { id } });
+  
+      if (!usuario) {
+        return res.status(404).json({ msg: "Usuário não encontrado" });
+      }
+  
+      res.status(200).json({
+        message: "Usuário atualizado com sucesso!",
+        usuario,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Falha na atualização do usuário" });
+    }
+  };
+  
--- backend/src/routes/usuariosRouter.js
+++ backend/src/routes/usuariosRouter.js
@@ -1,9 +1,10 @@
 import { Router} from "express";
-import {postUsuarios, loginUsuario} from "../controllers/usuariosController.js";
+import {postUsuarios, loginUsuario, updateUsuario} from "../controllers/usuariosController.js";
 const router = Router();
 
 // Rotas de usuários
 router.post("/usuarios/registro", postUsuarios);
 router.post("/usuarios/login", loginUsuario);
+router.put("/usuarios/:id", updateUsuario)
 
 export default router;
